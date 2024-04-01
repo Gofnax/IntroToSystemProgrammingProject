c9b5b6cb74efcf2ace665908825262404c293870
Emil <glater.emil@gmail.com>
2024-04-01 13:00:40 +0300

    Try again to add ignore

--- Forum.c
+++ Forum.c
@@ -5,6 +5,7 @@ int main(void)
 {
 	TimeStamp time;// = { .day = 4, .month = 11, .year = 1998, .hour = 23, .minute = 7 };
 	(void)initTime(&time);
-	printf("test");
+	printf("test\n");
+	printf("dlvtlm\n");
 	printf("%s\n", getTimeString(&time));
 }
